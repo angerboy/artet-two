bbdd775cf67abb668693a28de492d58e50208508
Matt Ehinger <mnehinger@gmail.com>
2019-04-28 20:51:07 +0100

    show test values

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,14 @@
+document.getElementById('inner-height').innerHTML = "window.innerHeight: " + window.innerHeight;
+document.getElementById('height').innerHTML = "screen.height: " + screen.height;
+
 // We listen to the resize event
 window.addEventListener('resize', () => {
 	// We execute the same script as before
 	let vh = window.innerHeight * 0.01;
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+	document.getElementById('inner-height').innerHTML = "window.innerHeight: " + window.innerHeight;
+	document.getElementById('height').innerHTML = "screen.height: " + screen.height;
 });
 
 // window.onresize = function() {
