423265f6f1fa60a99743ab18d878b6c7c61f3d73
Matt Ehinger <mnehinger@gmail.com>
2019-05-01 21:51:00 +0100

    console.debug

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,10 @@
+console.log("on load window.innerHeight: ", window.innerHeight);
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 window.addEventListener('resize', () => {
+	console.log("resize window.innerHeight: ", window.innerHeight);
 	let vh = window.innerHeight * 0.01;
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
